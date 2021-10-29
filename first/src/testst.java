f89cb28de306dd615b00891df40315397574f026
mgmgnomnom <bootyfulmemechild@gmail.com>
2021-10-29 22:52:26 +0800

    took back the files lol

---
+++ first/src/testst.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class testst {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("put a text hehe: ");  
+			String str= sc.nextLine();
+			String new_str = str.replaceAll("b", " :bingles: " );
+			System.out.println("bingle-ified text: " + new_str);
+		} 
+		System.out.println("copy paste in discord!!");
+    }
+}
